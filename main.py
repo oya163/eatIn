598af52861b88ad7b209767409cb1bbc0dc73af5
denmak1 <denmakmak1@gmail.com>
2017-11-13 17:08:36 -0500

    putting forms into separate py file

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from functools import wraps
 from passlib.hash import sha256_crypt
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
-from wtforms import Form, StringField, TextAreaField, PasswordField, SelectField, validators
-from wtforms.fields.html5 import DateField
 
 import models
+import forms
 
 # app/db config stuff
 app = Flask(__name__)
@@ -122,31 +121,31 @@ def get_chefspecial():
 
 
 # Sign Up Form Class
-class SignupForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=50)])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=50)])
-    email_id = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm Password')
-    usertype = SelectField('User Type', choices=[('customer','Customer'), ('chef','Chef')])
-    apartment_no = StringField('Apartment No', [validators.Length(min=1, max=50)])
-    street = StringField('Street', [validators.Length(min=1, max=50)])
-    city = StringField('City', [validators.Length(min=1, max=50)])
-    state = StringField('State', [validators.Length(min=2, max=10)])
-    zipcode = StringField('Zipcode', [validators.Length(min=4, max=10)])
-    country = StringField('Country', [validators.Length(min=2, max=50)])
-    phone_number = StringField('Phone', [validators.Length(min=4, max=50)])
-    # cuisine = SelectField('Cuisine', choices=[(x['cuisineid'],x['cuisine_name']) for x in get_chefspecial()], coerce=int)
+#class SignupForm(Form):
+#    first_name = StringField('First Name', [validators.Length(min=1, max=50)])
+#    last_name = StringField('Last Name', [validators.Length(min=1, max=50)])
+#    email_id = StringField('Email', [validators.Length(min=6, max=50)])
+#    password = PasswordField('Password', [
+#        validators.DataRequired(),
+#        validators.EqualTo('confirm', message='Passwords do not match')
+#    ])
+#    confirm = PasswordField('Confirm Password')
+#    usertype = SelectField('User Type', choices=[('customer','Customer'), ('chef','Chef')])
+#    apartment_no = StringField('Apartment No', [validators.Length(min=1, max=50)])
+#    street = StringField('Street', [validators.Length(min=1, max=50)])
+#    city = StringField('City', [validators.Length(min=1, max=50)])
+#    state = StringField('State', [validators.Length(min=2, max=10)])
+#    zipcode = StringField('Zipcode', [validators.Length(min=4, max=10)])
+#    country = StringField('Country', [validators.Length(min=2, max=50)])
+#    phone_number = StringField('Phone', [validators.Length(min=4, max=50)])
+#    cuisine = SelectField('Cuisine', choices=[(x['cuisineid'],x['cuisine_name']) for x in get_chefspecial()], coerce=int)
 
 
 # SignUp
 @app.route('/signup', methods=['GET', 'POST'])
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
-    form = SignupForm(request.form)
+    form = forms.SignupForm(request.form)
 
     if request.method == 'POST' and form.validate():
         fname     = form.first_name.data
@@ -200,31 +199,31 @@ def contactus():
     return render_template('contactus.html')
 
 
-class OrderPageForm(Form):
-    cuisineName = StringField('cuisine', [validators.Length(min=1, max=25)])
-    orderDate = StringField('datepicker')
-    comments = StringField('comments', [validators.Length(min=1, max=50)])
+#class OrderPageForm(Form):
+#    cuisineName = StringField('cuisine', [validators.Length(min=1, max=25)])
+#    orderDate = StringField('datepicker')
+#    comments = StringField('comments', [validators.Length(min=1, max=50)])
 
 
 @app.route('/orderpage', methods=['GET', 'POST'])
 def orderpage():
-    form = OrderPageForm(request.form)
+    form = forms.OrderPageForm(request.form)
     if request.method == 'POST' and form.validate():
         return render_template('orderpage.html')
     return render_template('orderpage.html', form=form)
 
 
 # Sign Up Form Class
-class DashboardOrderForm(Form):
-    order_name = StringField('What\'s your order', [validators.Length(min=1, max=50)])
-    requested_date = DateField('Requested date', format='%m/%d/%Y')
-    comments = TextAreaField('Comments', [validators.Length(min=0)])
+#class DashboardOrderForm(Form):
+#    order_name = StringField('What\'s your order', [validators.Length(min=1, max=50)])
+#    requested_date = DateField('Requested date', format='%m/%d/%Y')
+#    comments = TextAreaField('Comments', [validators.Length(min=0)])
 
 
 @app.route('/dashboard_order', methods=['GET', 'POST'])
 @is_logged_in
 def dashboard_order():
-    form = DashboardOrderForm(request.form)
+    form = forms.DashboardOrderForm(request.form)
     if request.method == 'POST' and form.validate():
         order_name = form.order_name.data
         requested_date = form.requested_date.data
