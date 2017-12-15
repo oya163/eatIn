bcc9148cb96b0238441e56aa616c7ffb475ed711
Oyashi <oyeshsin@hotmail.com>
2017-12-14 19:55:08 -0500

    Nothing major

--- main.py
+++ main.py
@@ -600,5 +600,5 @@ def update_session(user):
 
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port = 5050, debug = True)
-    #app.run(host = "127.0.0.1", port = 5050, debug = True)
+    # app.run(host = "0.0.0.0", port = 5050, debug = True)
+    app.run(host = "127.0.0.1", port = 5050, debug = False)
